b7e6bbb9c788adf7e378921fffac6df1653e22c3
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-19 18:56:59 -0600

    Create basic homepage content that matches provided mockup images and text

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '4rem', backgroundColor: '#8B5E3C', height: '100vh', color: 'white' }}>
+      <h1 style={{ fontSize: '3rem' }}>Coffee R Us</h1>
+      <p style={{ fontSize: '1.25rem' }}>The go to store for your coffee needs</p>
+    </div>
+  );
+}
+
+export default Home;
