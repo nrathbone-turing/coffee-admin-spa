5f2819556aa00cdbed1848e50ee6b2c3f566bcf6
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-19 19:36:38 -0600

    Add comments for clarity to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 // Import NavLink for navigation styling!
 import { NavLink } from 'react-router-dom';
 
+// just a simple horizontal navigation bar for now based on mockups
 function Header() {
   return (
-    <nav style={{ display: 'flex', gap: '2rem', padding: '1rem', backgroundColor: '#8B7E74' }}>
+    <nav 
+        style={{ 
+            display: 'flex', 
+            gap: '2rem', 
+            padding: '1rem', 
+            backgroundColor: '#8B7E74' 
+        }}
+    >
+      {/* NavLink applies an "active" style when route matches via white underline in this case */}
       <NavLink to="/" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none', color: 'white' })}>
         Home
       </NavLink>
