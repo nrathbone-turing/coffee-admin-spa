c541fcba88ca3a325f8110c41102ece14f815bae
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-19 19:39:44 -0600

    Refactor App test to remove duplicate Router

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,14 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('App', () => {
   it('renders navigation links', () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    render(<App />);
 
     expect(screen.getByText(/Home/i)).toBeInTheDocument();
     expect(screen.getByText(/Shop/i)).toBeInTheDocument();
